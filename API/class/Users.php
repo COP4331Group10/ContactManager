5858c6a3daed71b9617b82514fa4c6685efafc2c
lfernandogalvis <61099417+lfernandogalvis@users.noreply.github.com>
2020-05-21 22:23:33 -0400

    Update Users.php

--- API/class/Users.php
+++ API/class/Users.php
@@ -38,58 +38,5 @@ class Users
     		$data = [
 			    'login' => $this->_username,
 				'password' => $this->_usernamePW
-=======
- * @author Ngoc Nguyen
- *
- */
 
-include("DBConnection.php");
-class Contacts
-{
-    protected $db;
-    private $_id;
-    private $_login;
-    private $_password;
-
-    public function setLogin($Login) {
-        $this->_login = $Login;
-    }
-    public function setPassword($Password) {
-        $this->_password = $Password;
-    }
-
-    public function __construct() {
-        $this->db = new DBConnection();
-        $this->db = $this->db->returnConnection();
-   }
-
-
-    // create contact
-    public function createUser() {
-		try {
-    		$sql = "INSERT INTO Contacts (Login, Password)
-					VALUES (:login, :password)";
-    		$data = [
-			    'login' => $this->_login,
-				'password' => $this->_password,
->>>>>>> 62c3af5f479a9ddfeee0d17b8eaf8877397ad8d5
-			];
-	    	$stmt = $this->db->prepare($sql);
-	    	$stmt->execute($data);
-			$status = $stmt->rowCount();
-            return $status;
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> 62c3af5f479a9ddfeee0d17b8eaf8877397ad8d5
-		} catch (Exception $e) {
-    		die("There's an error in the query!");
-		}
-    }
-}
-<<<<<<< HEAD
-?>	
-=======
 ?>
->>>>>>> 62c3af5f479a9ddfeee0d17b8eaf8877397ad8d5
