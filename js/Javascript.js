6d5c54d508d8b84bec5182d9cf82403d901033bb
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2020-05-24 17:12:16 -0400

    called functions from html

--- js/Javascript.js
+++ js/Javascript.js
@@ -134,12 +134,6 @@ function goToAddContact()
 	window.location.href = "createNewContact.html";
 }
 
-function goToEditContact()
-{
-	window.location.href = "editContact.html";
-	// autofill the contacts information 4-5 pull requests probably.
-}
-
 function addContact()
 {
 	var firstName = document.getElementById("firstNameText").value;
@@ -193,7 +187,7 @@ function searchContact()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				document.getElementById("contactSearchResult").innerHTML = "Knightacts have been retrieved";
 				var jsonObject = JSON.parse( xhr.responseText );
 				
 				for( var i = 0; i < jsonObject.results.length; i++ )
@@ -216,27 +210,6 @@ function searchContact()
 	}	
 }
 
-function updateContact()
-{
-	//delete old 
-	//save new and add it to list 
-	window.location.href = "contactPage.html";
-}
-
-function deleteContact()
-{
-	var r = confirm("Are you sure you want to delete this Knightact?");
-	if (r == true) 
-	{
-		 // delete and redirect back to contactPage.html
-		 window.location.href = "contactPage.html";
-	}
-	else {
-  		return;
-	}
-	
-}
-
 function editPage()
 {
 	var firstName = document.getElementById("firstName").value;
@@ -247,15 +220,13 @@ function editPage()
 	var notesContact = document.getElementById("notesContact").value;
 	
 	//This should fill in the text boxes
-		firstName = document.getElementById("firstName").innerHTML;
-		lastName = document.getElementById("lastName").innerHTML;
-		emailContact = document.getElementById("emailContact").innerHTML;
-		phoneNumber = document.getElementById("phoneNumber").innerHTML;
-		addressContact = document.getElementById("addressContact").innerHTML;
-		notesContact = document.getElementById("notesContact").innerHTML;
-	
-
-	
+	firstName = document.getElementById("firstName").innerHTML;
+	lastName = document.getElementById("lastName").innerHTML;
+	emailContact = document.getElementById("emailContact").innerHTML;
+	phoneNumber = document.getElementById("phoneNumber").innerHTML;
+	addressContact = document.getElementById("addressContact").innerHTML;
+	notesContact = document.getElementById("notesContact").innerHTML;
+		
 	//This will allow the change
 	var jsonPayload = '{"First Name" : "' + firstName + '","Last Name" : "' + lastName + '","Email" : "' + emailContact + '","PhoneNumber" : "' + phoneNumber + '","Address" : "' + addressContact + '","Notes" : "' + notesContact + '","id" : "' + id + '","Email" : "' + email + '"}';
 	var url = urlBase + '/EditContact.' + extension;
@@ -269,7 +240,7 @@ function editPage()
 		{
 			if (this.readyState == 4 && this.status == 200)
 			{
-				document.getElementById("editResult").innerHTML = "Contact has been Updated";
+				document.getElementById("editResult").innerHTML = "Knightact has been Updated";
 				var jsonObject = JSON.parse( xhr.responseText );
 
 				if(jsonObject.results != undefined)
@@ -298,13 +269,13 @@ function editPage()
 	{
 		document.getElementById("editResult").innerHTML = err.message;
 	}
-
-
 }
 
 
 function editButton()
 {
+	window.location.href = "editContact.html"; //redirects to edit page
+
 	var jsonPayload = '{"id" : "' + id + '"}';
 	var url = urlBase + '/EditSearch.' + extension;
 
@@ -347,10 +318,10 @@ function editButton()
 	}
 }
 
-function deleteButton()
+function deleteContact()
 {
 	
-	var prompt = confirm("Are you sure you want to delete " + text);
+	var prompt = confirm("Are you sure you want to delete this Knightact?");
 	if(prompt)
 	{
 		var jsonPayload = '{"id" : "' + id + '"}';
@@ -362,7 +333,9 @@ function deleteButton()
 		try
 		{
 			xhr.send(jsonPayload);
-			location.reload();
+			//location.reload();
+			window.location.href = "contactPage.html";
+
 		}
 		catch(err)
 		{
