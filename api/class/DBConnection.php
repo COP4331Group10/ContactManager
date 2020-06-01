1d9f657b45b4ea43319c27978d1a5cdba52c8a81
Ngoc <54807762+nnguyen83@users.noreply.github.com>
2020-06-01 12:01:35 -0400

    Update DBConnection.php

--- api/class/DBConnection.php
+++ api/class/DBConnection.php
@@ -3,28 +3,28 @@
  * @package PHP Rest API(DBConnection)
  *
  * @author Ngoc Nguyen
- *   
+ *
  */
 
 // Database Connection
 class DBConnection {
-    private $_dbHostname = "localhost";
-    private $_dbName = "11567";
-    private $_dbUsername = "11567";
-    private $_dbPassword = "password";
-    private $_con;
+	private $_dbHostname = "localhost";
+	private $_dbName = "118794";
+	private $_dbUsername = "118794";
+	private $_dbPassword = FILL PASSWORD BEFORE RUNNING//"password";
+	private $_con;
 
-    public function __construct() {
-    	try {
-        	$this->_con = new PDO("mysql:host=$this->_dbHostname;dbname=$this->_dbName", $this->_dbUsername, $this->_dbPassword);    
-        	$this->_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    } catch(PDOException $e) {
-			  echo "Connection failed: " . $e->getMessage();
-		  }
-    }
-    // return Connection
-    public function returnConnection() {
-        return $this->_con;
-    }
+	public function __construct() {
+		try {
+			$this->_con = new PDO("mysql:host=$this->_dbHostname;dbname=$this->_dbName", $this->_dbUsername, $this->_dbPassword);
+			$this->_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+		}
+	}
+	// return Connection
+	public function returnConnection() {
+		return $this->_con;
+	}
 }
 ?>
