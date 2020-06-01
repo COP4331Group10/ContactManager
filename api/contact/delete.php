86d4d753f5d1f4da1eafa9dbe90be594cbd5979e
Ngoc <54807762+nnguyen83@users.noreply.github.com>
2020-06-01 12:03:52 -0400

    Update delete.php

--- api/contact/delete.php
+++ api/contact/delete.php
@@ -1,30 +1,38 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: DELETE");
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-include('../class/Contacts.php');
+include ('../class/Contacts.php');
 $contact = new Contacts();
-switch($requestMethod) {
-	case 'POST':
-		$empId = '';	
-		if($_GET['id']) {
+switch ($requestMethod)
+{
+	case 'DELETE':
+		$empId = '';
+		if ($_GET['id'])
+		{
 			$contactID = $_GET['id'];
 			$contact->setContactID($contactID);
 		}
+
 		$contactInfo = $contact->deleteContact();
-		if(!empty($contactInfo)) {
-		//   $js_encode = json_encode(array('status'=>TRUE, 'message'=>'Contact deleted Successfully.'), true);
-		  header("HTTP/1.1 200 OK");
-        } else {
-			// $js_encode = json_encode(array('status'=>FALSE, 'message'=>'Contact delete failed.'), true);
-			header("HTTP/1.1 404 Not Found");
-        }
+
+		if (!empty($contactInfo))
+		{
+			header("HTTP/1.0 200 OK");
+			$js_encode = json_encode(array('status'=>TRUE, 'message'=>'Contact deleted successfully'), true);
+		}
+		else
+		{
+			header("HTTP/1.0 409 Conflict");
+			$js_encode = json_encode(array('status'=>FALSE, 'message'=>'Contact deletetion failed'), true);
+		}
+
 		header('Content-Type: application/json');
 		echo $js_encode;
 		break;
 	default:
-	header("HTTP/1.0 405 Method Not Allowed");
-	break;
+		header("HTTP/1.0 405 Method Not Allowed");
+		break;
 }
 ?>
